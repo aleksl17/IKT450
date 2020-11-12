959781cf2fb4122c2f50a6add17746522f829e94
Aleksander Markus Lingstad <aleksl17@uia.no>
2020-11-12 14:09:57 +0100

    Added x_train cropped passthrough

--- input.py
+++ input.py
@@ -6,23 +6,32 @@
 import numpy
 import tensorflow as tf
 from image_shuffler import image_shuffler
+from image_shuffler import crop_centered
 
 
 def img_input(amount):
-    x_train = []
-    x_test = []
     y_train_shuffle = []
     y_test_shuffle = []
+    x_train_shuffled = []
+    x_test_shuffled = []
+    x_train_cropped = []
+    x_test_cropped = []
+
     (x_train_input, y_train_classify_input), (x_test_input, y_test_classify_input) = tf.keras.datasets.cifar10.load_data()
+
     for a in range(amount):
         x_train_image_shuffler, y_train_image_shuffler = image_shuffler(x_train_input[a])
-        x_train.append(x_train_image_shuffler)
+        x_train_shuffled.append(x_train_image_shuffler)
         y_train_shuffle.append(y_train_image_shuffler)
+        x_train_cropped = crop_centered(x_train_input[a])
+
     for b in range(amount // 5):
         x_test_image_shuffler, y_test_shuffle_image_shuffler = image_shuffler(x_test_input[b])
-        x_train.append(x_test_image_shuffler)
+        x_test_shuffled.append(x_test_image_shuffler)
         y_train_shuffle.append(y_test_shuffle_image_shuffler)
-    return x_train, y_train_classify_input, y_train_shuffle, x_test, y_test_classify_input, y_test_shuffle
+        x_train_cropped = crop_centered(x_train_input[b])
+
+    return x_train_cropped, x_train_shuffled, y_train_classify_input, y_train_shuffle, x_test_cropped, x_test_shuffled, y_test_classify_input, y_test_shuffle
 
 
-x_train, y_train_classify, y_train_shuffle, x_test, y_test_classify, y_test_shuffle = img_input(2)
+x_train, x_train_shuffle, y_train_classify, y_train_shuffle, x_test, x_test_shuffle, y_test_classify, y_test_shuffle = img_input(2)
