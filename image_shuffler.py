fb3b6c6bbde8666c473da88e9c1d634f4399bfb0
Aleksander Markus Lingstad <aleksl17@uia.no>
2020-11-12 11:27:15 +0100

    Added input manager file and image_shuffler function

--- image_shuffler.py
+++ image_shuffler.py
@@ -3,7 +3,7 @@ import random
 import tensorflow as tf
 
 # Configurable Global Variables
-crop_amount = 9  # Set to "-1" to crop ALL images.
+crop_amount = 9  # Set to "-1" to crop ALL images. # MOVE TO INPUT
 random.seed(69)
 
 # Global Variables
@@ -11,11 +11,11 @@ ds = []
 
 
 # Crops 3-dimensional image arrays towards center.
-def crop_centered(img, crop_x, crop_y):
+def crop_centered(img):
     x, y, c = img.shape
-    start_x = x // 2 - (crop_x // 2)
-    start_y = y // 2 - (crop_y // 2)
-    return img[start_x:start_x + crop_x, start_y:start_y + crop_y, :]
+    start_x = x//2-(30//2)
+    start_y = y//2-(30//2)
+    return img[start_x:start_x+30, start_y:start_y+30, :]
 
 
 # Shuffles image in a 3x3 fashion.
@@ -23,16 +23,17 @@ def shuffle_image(img):
     sections = []
     full_y = []
     random_head = list(range(0, 10))
-    # Divide picture into
-    print("Shape: ", img.shape)
+    # Divide 30x30 picture into 9, 10x10 sub-pictures
     for px in range(3):
         for py in range(3):
             section = img[10*px:10*px+10, 10*py:10*py+10]
             sections.append(section)
+    # Shuffle images
     shuffle = numpy.array(sections)
     combine = list(zip(shuffle, random_head))
     random.shuffle(combine)
     shuffle, random_head = zip(*combine)
+    # Convert number y to binary y.
     for rh in random_head:
         tmp_list = [0]*9
         tmp_list[rh] = 1
@@ -40,13 +41,18 @@ def shuffle_image(img):
     return shuffle, full_y
 
 
+def image_shuffler(img):
+    cropped_img = crop_centered(img)
+    x_train_shuffled, y_train_shuffled = shuffle_image(cropped_img)
+    return x_train_shuffled, y_train_shuffled
+
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
 
 # Crop Images
 if crop_amount == -1:
     crop_amount = len(x_train)
 for i in range(crop_amount):
-    tmp_crop_img = crop_centered(x_train[i], 30, 30)
+    tmp_crop_img = crop_centered(x_train[i])
     ds.append(tmp_crop_img)
 
-bs, y = shuffle_image(ds[1])
+bs, smol_y = shuffle_image(ds[1])
---
+++ input.py
@@ -0,0 +1,7 @@
+# TODO
+# loop through files
+# Read/write files
+# remake file
+
+def input(amount):
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
