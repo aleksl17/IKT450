9c933b53e674a402b1e46e923f3ba58b30a3828c
Aleksander Markus Lingstad <aleksl17@uia.no>
2020-11-16 14:04:29 +0100

    Finalized image_shuffler and added amount checker to input

--- image_shuffler.py
+++ image_shuffler.py
@@ -1,12 +1,6 @@
 import numpy
 import random
 
-# TODO
-# Move "deconstruct" and "reconstruct" code into functions
-# Optimize "de/reconstruct" code with loop
-# Consider moving labeling into separate function
-# Verify label code
-
 
 # Crops 3-dimensional image arrays towards a 30x30 center.
 def crop_centered(img):
@@ -18,44 +12,60 @@ def crop_centered(img):
 
 # Shuffles image in a 3x3 fashion.
 def shuffle_image(img):
-    sections = []
     full_y = []
     random_head = list(range(0, 10))
 
-    # Divide 30x30 picture into 9, 10x10 sub-pictures
-    for px in range(3):
-        for py in range(3):
-            section = img[10 * px:10 * px + 10, 10 * py:10 * py + 10]
-            sections.append(section)
+    # Deconstruct image via function
+    sections = deconstruct_image(img)
 
-    # Convert python list to numpy array.
-    all_sections = numpy.array(sections)
-
-    # Shuffle images
-    combine = list(zip(all_sections, random_head))
+    # Shuffle images with labels
+    combine = list(zip(sections, random_head))
     random.shuffle(combine)
-    all_sections, random_head = zip(*combine)
+    sections, random_head = zip(*combine)
 
-    # Convert array of numbers to binary location-based array
+    # Convert array of numbers to binary location-based array.
     for rh in random_head:
         tmp_list = [0] * 9
         tmp_list[rh] = 1
         full_y.extend(tmp_list)
 
-    # Reconstruct image
-    first_two = numpy.concatenate((all_sections[0], all_sections[1]), axis=1)
-    first_row = numpy.concatenate((first_two, all_sections[2]), axis=1)
-    second_two = numpy.concatenate((all_sections[3], all_sections[4]), axis=1)
-    second_row = numpy.concatenate((second_two, all_sections[5]), axis=1)
-    third_two = numpy.concatenate((all_sections[6], all_sections[7]), axis=1)
-    third_row = numpy.concatenate((third_two, all_sections[8]), axis=1)
+    # Reconstruct image via function.
+    reconstruct = reconstruct_image(sections)
+
+    return reconstruct, full_y
+
+
+# Deconstruct a single array item into a 9-long 10, 10, 3 image array.
+def deconstruct_image(img_item):
+    sections = []
+
+    # Divide 30x30 picture into 9, 10x10 sub-pictures.
+    for px in range(3):
+        for py in range(3):
+            section = img_item[10 * px:10 * px + 10, 10 * py:10 * py + 10]
+            sections.append(section)
+
+    # Convert list to numpy array
+    sections = numpy.array(sections)
+
+    return sections
+
+
+# Reconstructs a 9-long list of 10, 10, 3 images into a single 30, 30, 3 array item.
+def reconstruct_image(img_array):
+    first_two = numpy.concatenate((img_array[0], img_array[1]), axis=1)
+    first_row = numpy.concatenate((first_two, img_array[2]), axis=1)
+    second_two = numpy.concatenate((img_array[3], img_array[4]), axis=1)
+    second_row = numpy.concatenate((second_two, img_array[5]), axis=1)
+    third_two = numpy.concatenate((img_array[6], img_array[7]), axis=1)
+    third_row = numpy.concatenate((third_two, img_array[8]), axis=1)
     first_two_row = numpy.concatenate((first_row, second_row), axis=0)
     reconstruct = numpy.concatenate((first_two_row, third_row), axis=0)
 
     # Convert reconstructed image list to numpy array.
     reconstruct = numpy.array(reconstruct)
 
-    return reconstruct, full_y
+    return reconstruct
 
 
 # Crops and shuffles input image.
--- input.py
+++ input.py
@@ -1,8 +1,5 @@
 # TODO
-# loop through files
-# Read/write files
-# remake file
-# Make amount check and qol(if amount == -1 amount = 50000, if < 5, test_amount = 1, if > 50000, = 50000) Switch case maybe?
+# Make processed dataset file and file management
 
 import tensorflow as tf
 from matplotlib import pyplot
@@ -11,6 +8,18 @@ from image_shuffler import image_shuffler, crop_centered
 
 # Imports CIFAR10 dataset. Then performes cropping and shuffeling of dataset.
 def img_input(amount):
+    test_amount = 1
+    if amount <= -1:
+        amount = 50000
+        test_amount = 10000
+    elif amount <= 5:
+        test_amount = 1
+    elif amount > 50000:
+        amount = 50000
+        test_amount = 10000
+    else:
+        test_amount = amount // 5
+
     y_train_shuffled, y_test_shuffled, x_train_shuffled, x_test_shuffled, x_train_cropped, x_test_cropped = ([] for l in range(6))
 
     (x_train_input, y_train_classify_input), (x_test_input, y_test_classify_input) = tf.keras.datasets.cifar10.load_data()
@@ -21,7 +30,7 @@ def img_input(amount):
         y_train_shuffled.append(y_train_image_shuffler)
         x_train_cropped.append(crop_centered(x_train_input[atr]))
 
-    for ate in range(amount // 5):
+    for ate in range(test_amount):
         x_test_image_shuffler, y_test_image_shuffler = image_shuffler(x_test_input[ate])
         x_test_shuffled.append(x_test_image_shuffler)
         y_test_shuffled.append(y_test_image_shuffler)
