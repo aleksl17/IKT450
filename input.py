af0e9edd5547308a7b25ac7aa8d8ba38bf497852
Mariussuv <suvatnemarius@gmail.com>
2020-11-23 12:00:40 +0100

    inversion count

--- input.py
+++ input.py
@@ -1,10 +1,5 @@
-# TODO
-# Make processed dataset file and file management
-
 import numpy
-import random
 import tensorflow as tf
-from matplotlib import pyplot
 from image_shuffler import image_shuffler, crop_centered
 
 
@@ -22,7 +17,7 @@ def img_input(amount):
     else:
         test_amount = amount // 5
 
-    y_train_shuffled, y_test_shuffled, x_train_shuffled, x_test_shuffled, x_train_cropped, x_test_cropped = ([] for l in range(6))
+    y_train_shuffled, y_test_shuffled, y_train_classify_output, y_test_classify_output, x_train_shuffled, x_test_shuffled, x_train_cropped, x_test_cropped = ([] for l in range(8))
 
     (x_train_input, y_train_classify_input), (x_test_input, y_test_classify_input) = tf.keras.datasets.cifar10.load_data()
 
@@ -31,12 +26,14 @@ def img_input(amount):
         x_train_shuffled.append(x_train_image_shuffler)
         y_train_shuffled.append(y_train_image_shuffler)
         x_train_cropped.append(crop_centered(x_train_input[atr]))
+        y_train_classify_output.append(y_train_classify_input[atr])
 
     for ate in range(test_amount):
         x_test_image_shuffler, y_test_image_shuffler = image_shuffler(x_test_input[ate])
         x_test_shuffled.append(x_test_image_shuffler)
         y_test_shuffled.append(y_test_image_shuffler)
         x_test_cropped.append(crop_centered(x_test_input[ate]))
+        y_test_classify_output.append(y_test_classify_input[ate])
 
     # Convert from list to numpy array.
     x_train_cropped = numpy.array(x_train_cropped)
@@ -48,15 +45,4 @@ def img_input(amount):
     y_test_classify_input = numpy.array(y_test_classify_input)
     y_test_shuffled = numpy.array(y_test_shuffled)
 
-    return x_train_cropped, x_train_shuffled, y_train_classify_input, y_train_shuffled, x_test_cropped, x_test_shuffled, y_test_classify_input, y_test_shuffled
-
-
-# Syntax example and dataset visualization example.
-x_train, x_train_shuffle, y_train_classify, y_train_shuffle, x_test, x_test_shuffle, y_test_classify, y_test_shuffle = img_input(50)
-
-# tmp_rand = random.randint(0, 5)
-# pyplot.subplot(211)
-# pyplot.imshow(x_train[tmp_rand])
-# pyplot.subplot(212)
-# pyplot.imshow(x_train_shuffle[tmp_rand])
-# pyplot.show()
+    return x_train_cropped, x_train_shuffled, y_train_classify_output, y_train_shuffled, x_test_cropped, x_test_shuffled, y_test_classify_output, y_test_shuffled
